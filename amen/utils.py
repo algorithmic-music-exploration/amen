be668af46660f95cd1e782ac4d2c68ffa70d4a6b
Thor Kell <thor@tide-pool.ca>
2015-06-21 12:19:49 -0400

    Adding tests, fixing path

--- amen/utils.py
+++ amen/utils.py
@@ -6,6 +6,6 @@ import pkg_resources
 
 def example_audio_file():
     '''Get the included example file'''
-    path = 'examples.audio/amen.wav'
+    path = 'examples/audio/amen.wav'
     return pkg_resources.resource_filename(__name__, path)
 
