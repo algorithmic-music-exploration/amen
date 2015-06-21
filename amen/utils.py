4c1433854b6b69fdfb2ccde1bcf113fd6aa7a037
Thor Kell <thor@tide-pool.ca>
2015-06-21 12:04:39 -0400

    Adding example audio and function to get it

---
+++ amen/utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Utility functions'''
+
+import pkg_resources
+
+def example_audio_file():
+    '''Get the included example file'''
+    path = 'examples.audio/amen.wav'
+    return pkg_resources.resource_filename(__name__, path)
+
