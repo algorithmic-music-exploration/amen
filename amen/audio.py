ae93eb2ef78c8d4902ba29d5a10341159a28dcaa
Thor Kell <thor@tide-pool.ca>
2015-06-25 22:04:36 -0400

    Basic analysis setup

--- amen/audio.py
+++ amen/audio.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import librosa
+
 class Analysis(object):
     """
     Analyis object: should wrap the output from libRosa.
     """
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, sample_rate=22050):
         """
         Opens a file path, passes it to libRosa,
         and builds up the analysis.
         """
-        pass
+        self.file_path = file_path
+        y, sr = librosa.load(file_path, sr=sample_rate)
+        self.sample_rate = sr
+        self.raw_samples = y
