814ea36a743e7d97ea6eba2c2f7ecff48e94bd02
Thor Kell <thor@tide-pool.ca>
2015-07-01 21:54:16 -0400

    Building TimingList

--- amen/timing_list.py
+++ amen/timing_list.py
@@ -6,5 +6,10 @@ class TimingList(list):
     A list of TimeSlices.  
     """
 
-    def __init__(self, name):
+    def __init__(self, name, timings, audio):
+        # This assumes that we're going to get a list of tuples (start, duration) from librosa,
+        # which may or may not be true.
         self.name = name
+        for (start, duration) in timings:
+            slice = new TimeSlice(start, duration, audio)
+            self.append(slice)
