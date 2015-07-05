fef8944796460ae975d0ebc84a35b671023c4fea
Thor Kell <thor@tide-pool.ca>
2015-07-05 17:01:47 -0400

    Adding things to the base audio class

--- amen/audio.py
+++ amen/audio.py
@@ -3,17 +3,22 @@
 
 import librosa
 
-class Analysis(object):
+class Audio(object):
     """
-    Analyis object: should wrap the output from libRosa.
+    Audio object: should wrap the output from libRosa.
     """
 
-    def __init__(self, file_path, sample_rate=22050):
+    def __init__(self, file_path, convert_to_mono=False, sample_rate=22050):
         """
-        Opens a file path, passes it to libRosa,
-        and builds up the analysis.
+        Opens a file path, loads it with librosa.
         """
         self.file_path = file_path
-        y, sr = librosa.load(file_path, sr=sample_rate)
+        y, sr = librosa.load(file_path, mono=convert_to_mono, sr=sample_rate)
         self.sample_rate = sr
         self.raw_samples = y
+        if convert_to_mono:
+            self.num_channels = 1
+        else:
+            self.num_channels = 2
+        self.duration = len(self.raw_samples) / self.sample_rate
+
