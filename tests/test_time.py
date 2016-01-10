8a27c4b9ad2b9088c0f42568c164a470df517355
Thor Kell <thor@tide-pool.ca>
2016-01-10 11:52:13 -0500

    more wip on time tests

--- tests/test_time.py
+++ tests/test_time.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import pandas as pd
+import librosa
 from amen.audio import Audio
 from amen.utils import example_audio_file
 from amen.time import TimeSlice
@@ -39,3 +40,31 @@ audio = Audio(EXAMPLE_FILE)
 def test_offset_samples():
     res = audio.timings['beats'][0]._offset_samples(1, 2, (-1, 1), (-1, 1))
     assert(res.shape == (2, 3))
+
+samples, left_offset, right_offset  = audio.timings['beats'][5].get_samples()
+def test_get_samples_ends_in_zeros():
+    # Unsure about this one - need to talk to bmcfee, I think (or ignore it?)
+    pass
+
+def test_get_samples_shape():
+    duration = audio.timings['beats'][5].duration.delta * 1e-9
+    starting_sample, ending_sample = librosa.time_to_samples([0, duration], audio.sample_rate)
+
+    initial_length = ending_sample -starting_sample
+    left_offset_length = initial_length - left_offset[0] + left_offset[1]
+    right_offset_length = initial_length - right_offset[0] + right_offset[1]
+
+    assert(len(samples[0]) == left_offset_length)
+    assert(len(samples[1]) == right_offset_length)
+
+def test_get_samples_audio():
+    start = left_offset[0] * -1
+    end = len(samples[0]) - left_offset[1]
+    reset_samples = samples[0][start : end]
+
+    start = audio.timings['beats'][5].time.delta * 1e-9
+    duration = audio.timings['beats'][5].duration.delta * 1e-9
+    starting_sample, ending_sample = librosa.time_to_samples([start, start + duration], audio.sample_rate)
+    original_samples = audio.raw_samples[0, starting_sample : ending_sample]
+
+    assert(np.array_equiv(reset_samples, original_samples))
