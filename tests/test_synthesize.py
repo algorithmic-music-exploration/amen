70a835a36435ecd8c1b3fae8aadcffe2ba3f482b
Thor <thor@drip.fm>
2016-01-12 22:30:46 -0500

    Comments for future tests

--- tests/test_synthesize.py
+++ tests/test_synthesize.py
@@ -8,5 +8,7 @@ from amen.utils import example_audio_file
 EXAMPLE_FILE = example_audio_file()
 audio = Audio(EXAMPLE_FILE)
 
-# need tests for synthesize, and need to split the organizer function out
-# also need to solve the sample rate stuff with McFee
+# need to split the organizer function out and write a test for it.
+# need to test that synthesize gives back an Audio object
+# need to test that re-synthesizing all of amen.beats is (basically) the same as the input file
+# need to test that it fails if you try to write over 20 minutes of audio
