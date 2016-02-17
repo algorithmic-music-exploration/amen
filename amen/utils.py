e4c2fb5f883d5bbb61feae697838801dc62d9000
Thor Kell <thor@tide-pool.ca>
2016-02-16 21:52:48 -0500

    synthesize tests with mono

--- amen/utils.py
+++ amen/utils.py
@@ -8,3 +8,8 @@ def example_audio_file():
     '''Get the included example file'''
     path = 'examples/audio/amen.wav'
     return pkg_resources.resource_filename(__name__, path)
+
+def example_mono_audio_file():
+    '''Get the included example file'''
+    path = 'examples/audio/amen-mono.wav'
+    return pkg_resources.resource_filename(__name__, path)
