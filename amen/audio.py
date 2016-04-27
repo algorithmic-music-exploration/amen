3b53bbce2c30936a09ea1110da9af39273de2b6e
Thor Kell <thor@tide-pool.ca>
2016-04-26 20:48:05 -0400

    Add proper test for loading audio from raw_samples, fix a bug

--- amen/audio.py
+++ amen/audio.py
@@ -59,13 +59,14 @@ class Audio(object):
         if file_path:
             y, sr = librosa.load(file_path, mono=convert_to_mono, sr=sample_rate)
         elif raw_samples is not None:
-            y = raw_samples
+            # This assumes that we're passing in raw_samples
+            # directly from another Audio's raw_samples.
+            y = raw_samples[0]
             sr = sample_rate
 
         self.file_path = file_path
         self.sample_rate = float(sr)
         self.analysis_sample_rate = float(analysis_sample_rate)
-
         self.num_channels = y.ndim
         self.duration = librosa.get_duration(y=y, sr=sr)
 
@@ -202,9 +203,6 @@ class Audio(object):
         """
         feature = FeatureCollection()
         mono_samples = librosa.to_mono(self.analysis_samples)
-        print "a new file"
-        print type(self.analysis_samples), self.analysis_samples, self.analysis_samples.shape
-        print type(mono_samples), mono_samples, mono_samples.shape
         pitch_names = ['c', 'c#', 'd', 'eb', 'e', 'f', 'f#', 'g', 'ab', 'a', 'bb', 'b']
         chroma_cq = librosa.feature.chroma_cqt(mono_samples)
         for chroma, pitch in zip(chroma_cq, pitch_names):
