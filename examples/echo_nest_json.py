26752fb14bc850140b64aea73331c3cbaab79dfa
Thor Kell <thor@tide-pool.ca>
2017-05-14 15:05:17 -0400

    Simple echo next json example

---
+++ examples/echo_nest_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf=8
+
+"""
+echo_nest_json.py : Write Echo Nest JSON to a file
+"""
+
+import json
+from amen.audio import Audio
+from amen.utils import example_audio_file
+from amen.echo_nest_converter import AudioAnalysis
+
+audio_file = example_audio_file()
+audio = Audio(audio_file)
+
+remix_audio = AudioAnalysis(audio)
+with open('remix-json.json', 'w') as f:
+    json.dump(remix_audio.to_json(), f)
+
