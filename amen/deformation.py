f5393f72887972e9ffc6674af97d0e62b8db5129
Thor Kell <thor@tide-pool.ca>
2017-11-05 10:09:50 -0500

    add time strech

--- amen/deformation.py
+++ amen/deformation.py
@@ -10,6 +10,28 @@ import librosa
 
 from .audio import Audio
 
+
+def time_stretch(audio, rate):
+    """
+    Wraps librosa's `time_stretch` function, and returns a new Audio object.
+    Note that this folds to mono.
+
+    Parameters
+    ---------
+    audio : Audio
+        The Audio object to act on.
+
+    rate : float
+        The stretch factor
+        If rate is > 1, then the audio is sped up.
+        If rate is < 1, then the audio is slowed down.
+        A rate of `2.0` will result in audio that is twice as fast.
+    """
+    stretched = librosa.effects.time_stretch(librosa.to_mono(audio.raw_samples), rate=rate)
+    stretched_audio = Audio(raw_samples=stretched, sample_rate=audio.sample_rate)
+
+    return stretched_audio
+
 def harmonic_separation(audio, margin=3.0):
     """
     Wraps librosa's `harmonic` function, and returns a new Audio object.
@@ -17,6 +39,9 @@ def harmonic_separation(audio, margin=3.0):
 
     Parameters
     ---------
+    audio : Audio
+        The Audio object to act on.
+
     margin : float
         The larger the margin, the larger the separation.
         The default is `3.0`.
@@ -33,6 +58,9 @@ def percussive_separation(audio, margin=3.0):
 
     Parameters
     ---------
+    audio : Audio
+        The Audio object to act on.
+
     margin : float
         The larger the margin, the larger the separation.
         The default is `3.0`.
